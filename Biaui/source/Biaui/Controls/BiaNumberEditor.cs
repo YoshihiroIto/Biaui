608c1d1629aa6d44993da08af5568a70eb542535
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2018-11-03 12:54:08 +0900

    Add BiaNumberEditor

---
+++ Biaui/source/Biaui/Controls/BiaNumberEditor.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Biaui.Controls
+{
+    public class BiaNumberEditor : Control
+    {
+        static BiaNumberEditor()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BiaNumberEditor),
+                new FrameworkPropertyMetadata(typeof(BiaNumberEditor)));
+        }
+    }
+}
