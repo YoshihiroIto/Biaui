77cd695d85396b4dce804cb6972de68c1ba11000
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2018-11-07 10:02:19 +0900

    標準角丸値変更

--- Biaui/source/Biaui/Constants.cs
+++ Biaui/source/Biaui/Constants.cs
@@ -4,7 +4,7 @@ namespace Biaui
 {
     public static class Constants
     {
-        public static double BasicCornerRadiusPrim = 3.0;
+        public static double BasicCornerRadiusPrim = 4.0;
         public static CornerRadius BasicCornerRadius = new CornerRadius(BasicCornerRadiusPrim);
 
         public static double BasicOneLineHeight = 24.0;
