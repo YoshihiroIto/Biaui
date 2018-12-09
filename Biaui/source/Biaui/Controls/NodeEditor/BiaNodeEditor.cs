f023e13495508e50ba802e1948a37b0728a7192e
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2018-12-09 10:31:03 +0900

    BiaNodeEditor、スペースキー押下判定変更、マウスダウン時のみ見るようにした

--- Biaui/source/Biaui/Controls/NodeEditor/BiaNodeEditor.cs
+++ Biaui/source/Biaui/Controls/NodeEditor/BiaNodeEditor.cs
@@ -313,9 +313,6 @@ namespace Biaui.Controls.NodeEditor
             if (e.LeftButton != MouseButtonState.Pressed)
                 return;
 
-            if ((Win32Helper.GetAsyncKeyState(Win32Helper.VK_SPACE) & 0x8000) == 0)
-                return;
-
             if (_isScrolling == false)
                 return;
 
