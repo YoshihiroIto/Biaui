5ad2e9c7ba56a7b328acf5d5efb969f39f8d4f87
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2018-12-16 10:41:55 +0900

    リファクタリング

--- Biaui/source/Biaui/Internals/ImmutableRect.cs
+++ Biaui/source/Biaui/Internals/ImmutableRect.cs
@@ -8,24 +8,21 @@ namespace Biaui.Internals
     public readonly struct ImmutableRect
     {
         public readonly double X;
-
         public readonly double Y;
-
         public readonly double Width;
-
         public readonly double Height;
 
-        public ImmutableRect(double x, double y, double width, double height) =>
-            (X, Y, Width, Height) = (x, y, width, height);
+        public ImmutableRect(double x, double y, double width, double height)
+            => (X, Y, Width, Height) = (x, y, width, height);
 
-        public ImmutableRect(Point p, Size s) =>
-            (X, Y, Width, Height) = (p.X, p.Y, s.Width, s.Height);
+        public ImmutableRect(Point p, Size s)
+            => (X, Y, Width, Height) = (p.X, p.Y, s.Width, s.Height);
 
-        public static implicit operator ImmutableRect(Rect source) =>
-            new ImmutableRect(source.X, source.Y, source.Width, source.Height);
+        public static implicit operator ImmutableRect(Rect source)
+            => new ImmutableRect(source.X, source.Y, source.Width, source.Height);
 
-        public static implicit operator Rect(in ImmutableRect source) =>
-            new Rect(source.X, source.Y, source.Width, source.Height);
+        public static implicit operator Rect(in ImmutableRect source)
+            => new Rect(source.X, source.Y, source.Width, source.Height);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool IntersectsWith(in ImmutableRect target)
@@ -48,26 +45,31 @@ namespace Biaui.Internals
         }
 
         // ReSharper disable CompareOfFloatsByEqualityOperator
-        public static bool operator ==(in ImmutableRect source1, in ImmutableRect source2) =>
-            source1.X == source2.X && source1.Y == source2.Y && source1.Width == source2.Width &&
-            source1.Height == source2.Height;
+        public static bool operator ==(in ImmutableRect source1, in ImmutableRect source2)
+            => source1.X == source2.X &&
+               source1.Y == source2.Y &&
+               source1.Width == source2.Width &&
+               source1.Height == source2.Height;
         // ReSharper restore CompareOfFloatsByEqualityOperator
 
-        public static bool operator !=(in ImmutableRect source1, in ImmutableRect source2) => !(source1 == source2);
+        public static bool operator !=(in ImmutableRect source1, in ImmutableRect source2)
+            => !(source1 == source2);
 
-        public bool Equals(ImmutableRect other) => this == other;
+        public bool Equals(ImmutableRect other)
+            => this == other;
 
         public override bool Equals(object obj)
         {
             if (obj is ImmutableRect other)
-            {
                 return this == other;
-            }
 
             return false;
         }
 
-        public override int GetHashCode() =>
-            X.GetHashCode() ^ Y.GetHashCode() ^ Width.GetHashCode() ^ Height.GetHashCode();
+        public override int GetHashCode()
+            => X.GetHashCode() ^
+               Y.GetHashCode() ^
+               Width.GetHashCode() ^
+               Height.GetHashCode();
     }
 }
