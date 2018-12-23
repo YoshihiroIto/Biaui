61e9966f61105ba70874022ee6ca529b2b36871c
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2018-12-23 09:11:44 +0900

    ImmutablePoint => ImmutableVec2

--- Biaui/source/Biaui/Internals/ImmutableVec2.cs
+++ Biaui/source/Biaui/Internals/ImmutableVec2.cs
@@ -2,29 +2,29 @@
 
 namespace Biaui.Internals
 {
-    public readonly struct ImmutablePoint : IEquatable<ImmutablePoint>
+    public readonly struct ImmutableVec2 : IEquatable<ImmutableVec2>
     {
         public readonly double X;
         public readonly double Y;
 
-        public ImmutablePoint(double x, double y)
+        public ImmutableVec2(double x, double y)
             => (X, Y) = (x, y);
 
         // ReSharper disable CompareOfFloatsByEqualityOperator
-        public static bool operator ==(in ImmutablePoint source1, in ImmutablePoint source2)
+        public static bool operator ==(in ImmutableVec2 source1, in ImmutableVec2 source2)
             => source1.X == source2.X &&
                source1.Y == source2.Y;
         // ReSharper restore CompareOfFloatsByEqualityOperator
 
-        public static bool operator !=(in ImmutablePoint source1, in ImmutablePoint source2)
+        public static bool operator !=(in ImmutableVec2 source1, in ImmutableVec2 source2)
             => !(source1 == source2);
 
-        public bool Equals(ImmutablePoint other)
+        public bool Equals(ImmutableVec2 other)
             => this == other;
 
         public override bool Equals(object obj)
         {
-            if (obj is ImmutablePoint other)
+            if (obj is ImmutableVec2 other)
                 return this == other;
 
             return false;
