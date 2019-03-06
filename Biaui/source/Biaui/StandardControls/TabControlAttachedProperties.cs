ceee75b70ffff9581d629f877522450fb051a71a
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-03-06 09:41:36 +0700

    タブアイテム追加ボタンコマンドをバインディングで解決するようにした

--- Biaui/source/Biaui/StandardControls/TabControlAttachedProperties.cs
+++ Biaui/source/Biaui/StandardControls/TabControlAttachedProperties.cs
@@ -1,8 +1,5 @@
-﻿using System.Diagnostics;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
 using Biaui.Internals;
 
 namespace Biaui.StandardControls
@@ -60,34 +57,6 @@ namespace Biaui.StandardControls
         public static void SetAddCommandParameter(DependencyObject target, object value)
         {
             target.SetValue(AddCommandParameterProperty, value);
-        }
-
-        private void AddButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            var control = (DependencyObject) sender;
-
-            while (true)
-            {
-                var parent = VisualTreeHelper.GetParent(control);
-
-                Debug.Assert(parent != null);
-
-                if (parent is TabControl tabControl)
-                {
-                    var command = GetAddCommand(tabControl);
-                    if (command == null)
-                        return;
-
-                    var param = GetAddCommandParameter(tabControl);
-                    if (command.CanExecute(param) == false)
-                        return;
-
-                    command.Execute(param);
-                    return;
-                }
-
-                control = parent;
-            }
         }
     }
 }
