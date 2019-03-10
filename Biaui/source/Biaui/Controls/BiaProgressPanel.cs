7f332c77a057bb50cc7e8caae14f27723f2945be
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-03-10 09:54:28 +0700

    BiaProgressPanel追加

---
+++ Biaui/source/Biaui/Controls/BiaProgressPanel.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Biaui.Controls
+{
+    public class BiaProgressPanel : Control
+    {
+        static BiaProgressPanel()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BiaProgressPanel),
+                new FrameworkPropertyMetadata(typeof(BiaProgressPanel)));
+        }
+    }
+}
