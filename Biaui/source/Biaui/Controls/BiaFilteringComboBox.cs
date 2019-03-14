7274b518450482f78b8bbc441b257bac1b2b35be
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-03-14 11:46:03 +0700

    BiaFilteringComboBox追加

---
+++ Biaui/source/Biaui/Controls/BiaFilteringComboBox.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+
+namespace Biaui.Controls
+{
+    public class BiaFilteringComboBox : System.Windows.Controls.ComboBox
+    {
+        #region FilterWord
+        
+        public string FilterWord
+        {
+            get => _FilterWord;
+            set
+            {
+                if (value != _FilterWord)
+                    SetValue(FilterWordProperty, value);
+            }
+        }
+        
+        private string _FilterWord;
+        
+        public static readonly DependencyProperty FilterWordProperty =
+            DependencyProperty.Register(
+                nameof(FilterWord),
+                typeof(string),
+                typeof(BiaFilteringComboBox),
+                new PropertyMetadata(
+                    default,
+                    (s, e) =>
+                    {
+                        var self = (BiaFilteringComboBox) s;
+                        self._FilterWord = (string)e.NewValue;
+                    }));
+        
+        #endregion
+
+        static BiaFilteringComboBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BiaFilteringComboBox),
+                new FrameworkPropertyMetadata(typeof(BiaFilteringComboBox)));
+        }
+    }
+}
---
+++ Biaui/source/Biaui/Controls/BiaFilteringComboBoxEventHandler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using Biaui.Internals;
+
+namespace Biaui.Controls
+{
+    // ReSharper disable once UnusedMember.Global
+    public partial class BiaFilteringComboBoxEventHandler
+    {
+        private void ClearButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var button = (FrameworkElement) sender;
+            var wordTextBox = (TextBox) button.Tag;
+
+            wordTextBox.Focus();
+        }
+
+        private void ClearButton_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var button = (FrameworkElement) sender;
+            var wordTextBox = (TextBox) button.Tag;
+
+            wordTextBox.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                wordTextBox.Focus();
+            }));
+        }
+
+        private void WordTextBox_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Down &&
+                e.Key != Key.Up)
+                return;
+
+            var button = (FrameworkElement) sender;
+            var sv = button.Parent.Descendants<ScrollViewer>().First();
+
+            if (e.Key == Key.Down)
+                sv.ScrollToHome();
+            else
+                sv.ScrollToBottom();
+
+            // ※.VirtualizingStackPanelなためスクロールを済ませてからアイテムを探しに行く
+            sv.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                var content =
+                    e.Key == Key.Down
+                        ? button.Parent.Descendants<ComboBoxItem>().FirstOrDefault()
+                        : button.Parent.Descendants<ComboBoxItem>().LastOrDefault();
+
+                content?.Focus();
+            }));
+        }
+
+        private void Popup_OnClosed(object sender, EventArgs e)
+        {
+            var popup = (FrameworkElement) sender;
+            var parent = popup.GetParent<BiaFilteringComboBox>();
+
+            parent.FilterWord = "";
+
+            var expression = parent.GetBindingExpression(Selector.SelectedItemProperty);
+            expression?.UpdateTarget();
+        }
+    }
+}
