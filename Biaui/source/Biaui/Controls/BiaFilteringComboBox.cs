edb726dcd436febab5a5a326fe2104c7f64dc6e0
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-03-18 09:30:46 +0700

    BiaFilteringComboBox.IsEnableMouseWheel追加

--- Biaui/source/Biaui/Controls/BiaFilteringComboBox.cs
+++ Biaui/source/Biaui/Controls/BiaFilteringComboBox.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Input;
+using Biaui.Internals;
 
 namespace Biaui.Controls
 {
@@ -33,10 +35,46 @@ namespace Biaui.Controls
         
         #endregion
 
+        #region IsEnableMouseWheel
+        
+        public bool IsEnableMouseWheel
+        {
+            get => _IsEnableMouseWheel;
+            set
+            {
+                if (value != _IsEnableMouseWheel)
+                    SetValue(IsEnableMouseWheelProperty, value);
+            }
+        }
+        
+        private bool _IsEnableMouseWheel = true;
+        
+        public static readonly DependencyProperty IsEnableMouseWheelProperty =
+            DependencyProperty.Register(
+                nameof(IsEnableMouseWheel),
+                typeof(bool),
+                typeof(BiaFilteringComboBox),
+                new PropertyMetadata(
+                    Boxes.BoolTrue,
+                    (s, e) =>
+                    {
+                        var self = (BiaFilteringComboBox) s;
+                        self._IsEnableMouseWheel = (bool)e.NewValue;
+                    }));
+        
+        #endregion
+
         static BiaFilteringComboBox()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(BiaFilteringComboBox),
                 new FrameworkPropertyMetadata(typeof(BiaFilteringComboBox)));
+        }
+
+        protected override void OnPreviewMouseWheel(MouseWheelEventArgs e)
+        {
+            base.OnPreviewMouseWheel(e);
+
+            e.Handled = IsEnableMouseWheel == false;
         }
     }
 }
