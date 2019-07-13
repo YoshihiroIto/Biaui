52c2f370fee04f9c268d46fd43f7db91636f4efa
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-07-13 10:06:35 +0900

    ベジェカーブオブジェクトをキャッシュするように変更

--- Biaui/source/Biaui/Internals/DrawingContextExtensions.cs
+++ Biaui/source/Biaui/Internals/DrawingContextExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Media;
+using Biaui.Controls.NodeEditor;
+using Jewelry.Collections;
 
 namespace Biaui.Internals
 {
@@ -8,19 +10,38 @@ namespace Biaui.Internals
     {
         internal static void DrawBezier(this DrawingContext dc, Point[] bezierPoints, Pen pen)
         {
-            var pf = new PathFigure
+            DrawBezier(dc, bezierPoints[0], bezierPoints[1], bezierPoints[2], bezierPoints[2], pen);
+
+        }
+
+        private static readonly LruCache<int, PathGeometry> _bezierCache = new LruCache<int, PathGeometry>(10000, false);
+
+        internal static void DrawBezier(this DrawingContext dc, Point pos1, Point pos1C, Point pos2C, Point pos2, Pen pen)
+        {
+            var h = (pos1, pos1C, pos2C, pos2).GetHashCode();
+
+            var c = _bezierCache.GetOrAdd(h, _ =>
             {
-                StartPoint = bezierPoints[0]
-            };
+                var pf = new PathFigure
+                {
+                    StartPoint = pos1
+                };
 
-            var bs = new BezierSegment(bezierPoints[1], bezierPoints[2], bezierPoints[3], true);
+                var bs = new BezierSegment(pos1C, pos2C, pos2, true);
+                bs.Freeze();
 
-            pf.Segments.Add(bs);
+                pf.Segments.Add(bs);
+                pf.Freeze();
 
-            var curve = new PathGeometry();
-            curve.Figures.Add(pf);
+                var curve = new PathGeometry();
+                curve.Figures.Add(pf);
+                curve.Freeze();
 
-            dc.DrawGeometry(null, pen, curve);
+
+                return curve;
+            });
+
+            dc.DrawGeometry(null, pen, c);
         }
 
         internal static void DrawCircle(this DrawingContext dc, Brush brush, Pen pen, Point pos, double radius)
