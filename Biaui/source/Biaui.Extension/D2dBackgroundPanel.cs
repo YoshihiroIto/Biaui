b7d291d11ea792d24502b37a789b032d05c62895
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-07-14 16:25:03 +0900

    Add D2dBackgroundPanelGenerator

--- Biaui/source/Biaui.Extension/D2dBackgroundPanel.cs
+++ Biaui/source/Biaui.Extension/D2dBackgroundPanel.cs
@@ -1,10 +1,18 @@
-﻿using Biaui.Environment;
+﻿using Biaui.Controls.NodeEditor;
+using Biaui.Environment;
 using SharpDX.Direct2D1;
 
 namespace Biaui.Extension
 {
     public class D2dBackgroundPanel : D2dControl.D2dControl, IBackgroundPanel
     {
+        private readonly BiaNodeEditor _parent;
+
+        public D2dBackgroundPanel(BiaNodeEditor parent)
+        {
+            _parent = parent;
+        }
+
         public void Invalidate()
         {
         }
---
+++ Biaui/source/Biaui.Extension/D2dBackgroundPanelGenerator.cs
@@ -0,0 +1,11 @@
+﻿using Biaui.Controls.NodeEditor;
+using Biaui.Environment;
+
+namespace Biaui.Extension
+{
+    public class D2dBackgroundPanelGenerator
+    {
+        public IBackgroundPanel Generate(BiaNodeEditor parent)
+            => new D2dBackgroundPanel(parent);
+    }
+}
