6d75727413dfaee983415137c90a4cb92823f142
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-07-14 15:32:49 +0900

    Add D2dBackgroundPanel

---
+++ Biaui/source/Biaui.Extension/D2dBackgroundPanel.cs
@@ -0,0 +1,16 @@
+﻿using Biaui.Environment;
+using SharpDX.Direct2D1;
+
+namespace Biaui.Extension
+{
+    public class D2dBackgroundPanel : D2dControl.D2dControl, IBackgroundPanel
+    {
+        public void Invalidate()
+        {
+        }
+
+        public override void Render(DeviceContext target)
+        {
+        }
+    }
+}
