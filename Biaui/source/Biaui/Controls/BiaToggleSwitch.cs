30789b5c0601d8f6d6c0b259b1fe6569b135d04b
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-12-11 18:26:24 +0900

    Add BiaToggleSwitch

---
+++ Biaui/source/Biaui/Controls/BiaToggleSwitch.cs
@@ -0,0 +1,104 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Biaui.Internals;
+
+namespace Biaui.Controls
+{
+    public class BiaToggleSwitch : ContentControl
+    {
+        #region IsChecked
+
+        public bool IsChecked
+        {
+            get => _IsChecked;
+            set
+            {
+                if (value != _IsChecked)
+                    SetValue(IsCheckedProperty, Boxes.Bool(value));
+            }
+        }
+
+        private bool _IsChecked;
+
+        public static readonly DependencyProperty IsCheckedProperty =
+            DependencyProperty.Register(nameof(IsChecked), typeof(bool), typeof(BiaToggleSwitch),
+                new FrameworkPropertyMetadata(
+                    Boxes.BoolFalse,
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                    (s, e) =>
+                    {
+                        var self = (BiaToggleSwitch) s;
+                        self._IsChecked = (bool) e.NewValue;
+                    })
+                {
+                    BindsTwoWayByDefault = true
+                }
+            );
+
+        #endregion
+
+        #region EnabledLabel
+        
+        public string EnabledLabel
+        {
+            get => _EnabledLabel;
+            set
+            {
+                if (value != _EnabledLabel)
+                    SetValue(EnabledLabelProperty, value);
+            }
+        }
+        
+        private string _EnabledLabel;
+        
+        public static readonly DependencyProperty EnabledLabelProperty =
+            DependencyProperty.Register(
+                nameof(EnabledLabel),
+                typeof(string),
+                typeof(BiaToggleSwitch),
+                new PropertyMetadata(
+                    default,
+                    (s, e) =>
+                    {
+                        var self = (BiaToggleSwitch) s;
+                        self._EnabledLabel = (string)e.NewValue;
+                    }));
+        
+        #endregion
+
+        #region DisabledLabel
+        
+        public string DisabledLabel
+        {
+            get => _DisabledLabel;
+            set
+            {
+                if (value != _DisabledLabel)
+                    SetValue(DisabledLabelProperty, value);
+            }
+        }
+        
+        private string _DisabledLabel;
+        
+        public static readonly DependencyProperty DisabledLabelProperty =
+            DependencyProperty.Register(
+                nameof(DisabledLabel),
+                typeof(string),
+                typeof(BiaToggleSwitch),
+                new PropertyMetadata(
+                    default,
+                    (s, e) =>
+                    {
+                        var self = (BiaToggleSwitch) s;
+                        self._DisabledLabel = (string)e.NewValue;
+                    }));
+        
+        #endregion
+
+        static BiaToggleSwitch()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BiaToggleSwitch),
+                new FrameworkPropertyMetadata(typeof(BiaToggleSwitch)));
+        }
+    }
+}
