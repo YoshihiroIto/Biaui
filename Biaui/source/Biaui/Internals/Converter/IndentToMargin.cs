201e5de59f1180cc39cc9f1e3cf2d754cc1b623f
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-12-27 08:39:25 +0900

    Reduced heap allocation

--- Biaui/source/Biaui/Internals/Converter/IndentToMargin.cs
+++ Biaui/source/Biaui/Internals/Converter/IndentToMargin.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using Jewelry.Collections;
 
 namespace Biaui.Internals.Converter
 {
@@ -22,8 +23,23 @@ namespace Biaui.Internals.Converter
                 if (values[1] is double)
                     length = (double) values[1];
 
-            return new Thickness(length * item.GetDepth(), 0, 0, 0);
+            var k = length * item.GetDepth();
+
+            // ReSharper disable once CompareOfFloatsByEqualityOperator
+            if (k == 0)
+                return Boxes.Thickness0;
+
+            if (_thicknessCache.TryGetValue(k, out var result) == false)
+            {
+                result = new Thickness(k, 0, 0, 0);
+
+                _thicknessCache.Add(k, result);
+            }
+
+            return result;
         }
+
+        private static readonly LruCache<double, object> _thicknessCache = new LruCache<double, object>(8, false);
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
