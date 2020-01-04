5bf076d5e2101f3e179984dab11f512860f98bf1
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-01-04 21:04:34 +0900

    Improve ScrollViewer style

---
+++ Biaui/source/Biaui/StandardControls/ScrollViewerAttachedProperties.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using Biaui.Internals;
+
+namespace Biaui.StandardControls
+{
+    public class ScrollViewerAttachedProperties
+    {
+        public static readonly DependencyProperty HorizontalScrollBarOpacityProperty =
+            DependencyProperty.RegisterAttached(
+                "HorizontalScrollBarOpacity",
+                typeof(double),
+                typeof(ScrollViewerAttachedProperties),
+                new FrameworkPropertyMetadata(Boxes.Double0));
+
+        public static double GetHorizontalScrollBarOpacityScrollBar(DependencyObject target)
+        {
+            return (double) target.GetValue(HorizontalScrollBarOpacityProperty);
+        }
+
+        public static void SetHorizontalScrollBarOpacity(DependencyObject target, double value)
+        {
+            target.SetValue(HorizontalScrollBarOpacityProperty, Boxes.Double(value));
+        }
+
+        public static readonly DependencyProperty VerticalScrollBarOpacityProperty =
+            DependencyProperty.RegisterAttached(
+                "VerticalScrollBarOpacity",
+                typeof(double),
+                typeof(ScrollViewerAttachedProperties),
+                new FrameworkPropertyMetadata(Boxes.Double0));
+
+        public static double GetVerticalScrollBarOpacity(DependencyObject target)
+        {
+            return (double) target.GetValue(VerticalScrollBarOpacityProperty);
+        }
+
+        public static void SetVerticalScrollBarOpacity(DependencyObject target, double value)
+        {
+            target.SetValue(VerticalScrollBarOpacityProperty, Boxes.Double(value));
+        }
+    }
+}
---
+++ Biaui/source/Biaui/StandardControls/ScrollViewerEventHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Biaui.Internals;
+
+namespace Biaui.StandardControls
+{
+    public partial class ScrollViewerEventHandler
+    {
+        private const double Response = 150.0;
+        private const double MaxOpacity = 0.6;
+        private const double BarWidth = 8.0;
+
+        private void OnPreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            var self = ((FrameworkElement) sender).GetParent<ScrollViewer>();
+
+            var pos = e.GetPosition(self);
+
+            var width = (1.0, self.ActualWidth - BarWidth).Max();
+            var height = (1.0, self.ActualHeight - BarWidth).Max();
+
+            var xr = (Response, width).Min();
+            var yr = (Response, height).Min();
+
+            var xir = width - xr;
+            var yir = height - yr;
+
+            var xd = NumberHelper.Clamp01((pos.X - xir) / xr);
+            var yd = NumberHelper.Clamp01((pos.Y - yir) / yr);
+
+            var xo = xd * MaxOpacity;
+            var yo = yd * MaxOpacity;
+
+            self.SetValue(ScrollViewerAttachedProperties.VerticalScrollBarOpacityProperty, xo);
+            self.SetValue(ScrollViewerAttachedProperties.HorizontalScrollBarOpacityProperty, yo);
+        }
+
+        private void OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            var self = ((FrameworkElement) sender).GetParent<ScrollViewer>();
+
+            self.SetValue(ScrollViewerAttachedProperties.VerticalScrollBarOpacityProperty, Boxes.Double0);
+            self.SetValue(ScrollViewerAttachedProperties.HorizontalScrollBarOpacityProperty, Boxes.Double0);
+        }
+    }
+}
