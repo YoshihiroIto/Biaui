06335bdee694ceb4c8596c2e71dc193e0da1747b
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-01-14 16:42:21 +0900

    using System.Windows.Controls.Primitives;

--- Biaui/source/Biaui.Support/HighResponseExpanderBehavior.cs
+++ Biaui/source/Biaui.Support/HighResponseExpanderBehavior.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
 using Biaui.Controls;
