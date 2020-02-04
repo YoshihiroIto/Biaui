9fcb9f42c184a1c7b8e7f022410e2bacee78e0cd
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-02-04 22:09:33 +0900

    DependencyObjectExtensions.Descendants(): Reduce heap memory allocation

--- Biaui/source/Biaui/Internals/DependencyObjectExtensions.cs
+++ Biaui/source/Biaui/Internals/DependencyObjectExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
@@ -16,10 +18,8 @@ namespace Biaui.Internals
                 throw new ArgumentNullException(nameof(obj));
 
             if (obj is Popup popup)
-            {
                 if (popup.Child != null)
                     yield return popup.Child;
-            }
 
             var count = VisualTreeHelper.GetChildrenCount(obj);
             if (count == 0)
@@ -37,13 +37,48 @@ namespace Biaui.Internals
             if (obj == null)
                 throw new ArgumentNullException(nameof(obj));
 
-            foreach (var child in obj.Children())
-            {
-                yield return child;
+            var children = ArrayPool<DependencyObject>.Shared.Rent(obj.MaximumChildrenCount());
 
-                foreach (var grandChild in child.Descendants())
-                    yield return grandChild;
+            try
+            {
+                var childrenCount = obj.Children(children);
+
+                for (var i = 0; i != childrenCount; ++i)
+                {
+                    var child = children[i];
+
+                    yield return child;
+
+                    foreach (var grandChild in child.Descendants())
+                        yield return grandChild;
+                }
             }
+            finally
+            {
+                ArrayPool<DependencyObject>.Shared.Return(children);
+            }
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int MaximumChildrenCount(this DependencyObject obj)
+        {
+            return VisualTreeHelper.GetChildrenCount(obj) + 1;
+        }
+
+        private static int Children(this DependencyObject obj, Span<DependencyObject> result)
+        {
+            var count = 0;
+
+            if (obj is Popup popup)
+                if (popup.Child != null)
+                    result[count++] = popup.Child;
+
+            var childrenCount = VisualTreeHelper.GetChildrenCount(obj);
+
+            for (var i = 0; i != childrenCount; i++)
+                result[count++] = VisualTreeHelper.GetChild(obj, i);
+
+            return count;
         }
 
         public static T? GetParent<T>(this DependencyObject self) where T : class
