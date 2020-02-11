64e36d39bf7e7d53cac1215f6962fc600e166ee3
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-02-11 16:06:55 +0900

    Add some MathF methods

--- Biaui/source/Biaui/Internals/MathF.cs
+++ Biaui/source/Biaui/Internals/MathF.cs
@@ -6,8 +6,20 @@ namespace Biaui.Internals
 #if !NETCOREAPP3_1
     internal static class MathF
     {
+        // ReSharper disable once InconsistentNaming
+        public const float PI = 3.14159265358979f;
+        
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float Sqrt(float v) => (float) Math.Sqrt(v);
+        public static float Sqrt(float d) => (float) Math.Sqrt(d);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Atan2(float y, float x) => (float) Math.Atan2(y, x);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Sin(float a) => (float) Math.Sin(a);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Cos(float a) => (float) Math.Cos(a);
     }
 #endif
 }
