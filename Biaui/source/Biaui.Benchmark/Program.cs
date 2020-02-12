6cd83bf6504e3bfcf4f46da36d4c4333dd02b21f
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-02-13 08:11:24 +0900

    Remove unnecessary using

--- Biaui/source/Biaui.Benchmark/Program.cs
+++ Biaui/source/Biaui.Benchmark/Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 
 namespace Biaui.Benchmark
 {
     class Program
     {
+        // ReSharper disable once UnusedParameter.Local
         static void Main(string[] args)
         {
             BenchmarkRunner.Run<BiaNodeEditorHelperTest>();
