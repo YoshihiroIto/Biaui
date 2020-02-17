b13bc703ee63a1104b2398c955ddd812e20fd856
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-02-17 14:15:05 +0900

    Remove unnecessary unchecked blocks

--- Biaui/source/Biaui/Caches.cs
+++ Biaui/source/Biaui/Caches.cs
@@ -9,121 +9,106 @@ namespace Biaui
     {
         public static Pen GetPen(ByteColor color, double thickness)
         {
-            unchecked
-            {
-                var hashCode = HashCodeMaker.Make(color, thickness);
+            var hashCode = HashCodeMaker.Make(color, thickness);
 
-                if (_borderPens.TryGetValue(hashCode, out var p))
-                    return p;
-
-                p = new Pen(GetSolidColorBrush(color), thickness);
-                p.Freeze();
-
-                _borderPens.Add(hashCode, p);
-
+            if (_borderPens.TryGetValue(hashCode, out var p))
                 return p;
-            }
+
+            p = new Pen(GetSolidColorBrush(color), thickness);
+            p.Freeze();
+
+            _borderPens.Add(hashCode, p);
+
+            return p;
         }
 
         public static Pen GetCapPen(ByteColor color, double thickness)
         {
-            unchecked
-            {
-                var hashCode = HashCodeMaker.Make(color, thickness);
+            var hashCode = HashCodeMaker.Make(color, thickness);
 
-                if (_capPens.TryGetValue(hashCode, out var p))
-                    return p;
-
-                p = new Pen(GetSolidColorBrush(color), thickness)
-                {
-                    StartLineCap = PenLineCap.Round,
-                    EndLineCap = PenLineCap.Round
-                };
-                p.Freeze();
-
-                _capPens.Add(hashCode, p);
-
+            if (_capPens.TryGetValue(hashCode, out var p))
                 return p;
-            }
+
+            p = new Pen(GetSolidColorBrush(color), thickness)
+            {
+                StartLineCap = PenLineCap.Round,
+                EndLineCap = PenLineCap.Round
+            };
+            p.Freeze();
+
+            _capPens.Add(hashCode, p);
+
+            return p;
         }
 
         public static Pen GetDashedPen(ByteColor color, double thickness)
         {
-            unchecked
-            {
-                var hashCode = HashCodeMaker.Make(color, thickness);
+            var hashCode = HashCodeMaker.Make(color, thickness);
 
-                if (_dashedPens.TryGetValue(hashCode, out var p))
-                    return p;
-
-                p = new Pen(GetSolidColorBrush(color), thickness)
-                {
-                    DashStyle = DashStyles.Dash
-                };
-                p.Freeze();
-
-                _dashedPens.Add(hashCode, p);
-
+            if (_dashedPens.TryGetValue(hashCode, out var p))
                 return p;
-            }
+
+            p = new Pen(GetSolidColorBrush(color), thickness)
+            {
+                DashStyle = DashStyles.Dash
+            };
+            p.Freeze();
+
+            _dashedPens.Add(hashCode, p);
+
+            return p;
         }
 
         public static SolidColorBrush GetSolidColorBrush(ByteColor color)
         {
-            unchecked
-            {
-                var hashCode = HashCodeMaker.Make(color);
+            var hashCode = HashCodeMaker.Make(color);
 
-                if (_solidColorBrushes.TryGetValue(hashCode, out var b))
-                    return b;
-
-                b = new SolidColorBrush(color.ToColor());
-                b.Freeze();
-
-                _solidColorBrushes.Add(hashCode, b);
-
+            if (_solidColorBrushes.TryGetValue(hashCode, out var b))
                 return b;
-            }
+
+            b = new SolidColorBrush(color.ToColor());
+            b.Freeze();
+
+            _solidColorBrushes.Add(hashCode, b);
+
+            return b;
         }
 
         public static Geometry GetClipGeom(Visual visual, double w, double h, double cornerRadius, bool isWidthBorder)
         {
-            unchecked
-            {
-                var hashCode = HashCodeMaker.Make(w, h, cornerRadius, isWidthBorder);
+            var hashCode = HashCodeMaker.Make(w, h, cornerRadius, isWidthBorder);
 
-                if (_clipGeoms.TryGetValue(hashCode, out var c))
-                    return c;
-
-                if (isWidthBorder)
-                {
-                    c = new RectangleGeometry
-                    {
-                        RadiusX = cornerRadius,
-                        RadiusY = cornerRadius,
-                        Rect = visual.RoundLayoutRect(
-                            FrameworkElementExtensions.BorderWidth * 0.5,
-                            FrameworkElementExtensions.BorderWidth * 0.5,
-                            w - FrameworkElementExtensions.BorderWidth,
-                            h - FrameworkElementExtensions.BorderWidth)
-                    };
-                }
-                else
-                {
-                    c = new RectangleGeometry
-                    {
-                        RadiusX = cornerRadius,
-                        RadiusY = cornerRadius,
-                        Rect = visual.RoundLayoutRect(0, 0, w, h)
-                    };
-                }
-
-                c.Freeze();
-
-                _clipGeoms.Add(hashCode, c);
-
+            if (_clipGeoms.TryGetValue(hashCode, out var c))
                 return c;
+
+            if (isWidthBorder)
+            {
+                c = new RectangleGeometry
+                {
+                    RadiusX = cornerRadius,
+                    RadiusY = cornerRadius,
+                    Rect = visual.RoundLayoutRect(
+                        FrameworkElementExtensions.BorderWidth * 0.5,
+                        FrameworkElementExtensions.BorderWidth * 0.5,
+                        w - FrameworkElementExtensions.BorderWidth,
+                        h - FrameworkElementExtensions.BorderWidth)
+                };
             }
+            else
+            {
+                c = new RectangleGeometry
+                {
+                    RadiusX = cornerRadius,
+                    RadiusY = cornerRadius,
+                    Rect = visual.RoundLayoutRect(0, 0, w, h)
+                };
+            }
+
+            c.Freeze();
+
+            _clipGeoms.Add(hashCode, c);
+
+            return c;
         }
 
         private static readonly Dictionary<long, RectangleGeometry> _clipGeoms = new Dictionary<long, RectangleGeometry>();
