42a9e732f96882f1bcf8b47b64350fc01a32e76b
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-02-22 23:55:11 +0900

    Add BiaHyperlinkButton

--- Biaui/source/Biaui.Showcase/MainWindow.xaml.cs
+++ Biaui/source/Biaui.Showcase/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace Biaui.Showcase
+﻿using System.Diagnostics;
+using System.Windows;
+using Biaui.Controls;
+
+namespace Biaui.Showcase
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
@@ -10,6 +14,15 @@
             InitializeComponent();
 
             DataContext = new Data();
+        }
+
+        private void BiaButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var b = (BiaHyperlinkButton) sender;
+            
+            using var proc = Process.Start(new ProcessStartInfo("cmd", $"/c start {b.Content}") {CreateNoWindow = true});
+
+            proc?.WaitForExit();
         }
     }
 }
---
+++ Biaui/source/Biaui/Controls/BiaHyperlinkButton.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using Biaui.Internals;
+
+namespace Biaui.Controls
+{
+    public class BiaHyperlinkButton : BiaButtonBase
+    {
+        #region Content
+
+        public string? Content
+        {
+            get => _content;
+            set
+            {
+                if (value != _content)
+                    SetValue(ContentProperty, value);
+            }
+        }
+
+        private string? _content;
+
+        public static readonly DependencyProperty ContentProperty =
+            DependencyProperty.Register(nameof(Content), typeof(string), typeof(BiaHyperlinkButton),
+                new FrameworkPropertyMetadata(
+                    default(string),
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.SubPropertiesDoNotAffectRender,
+                    (s, e) =>
+                    {
+                        var self = (BiaHyperlinkButton) s;
+                        self._content = (string) e.NewValue;
+                        self.UpdateSize();
+                    }));
+
+        #endregion
+
+        #region TextTrimming
+
+        public BiaTextTrimmingMode TextTrimming
+        {
+            get => _TextTrimming;
+            set
+            {
+                if (value != _TextTrimming)
+                    SetValue(TextTrimmingProperty, Boxes.TextTrimming(value));
+            }
+        }
+
+        private BiaTextTrimmingMode _TextTrimming = BiaTextTrimmingMode.Standard;
+
+        public static readonly DependencyProperty TextTrimmingProperty =
+            DependencyProperty.Register(
+                nameof(TextTrimming),
+                typeof(BiaTextTrimmingMode),
+                typeof(BiaHyperlinkButton),
+                new FrameworkPropertyMetadata(
+                    Boxes.TextTrimmingModeStandard,
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault |
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.SubPropertiesDoNotAffectRender,
+                    (s, e) =>
+                    {
+                        var self = (BiaHyperlinkButton) s;
+                        self._TextTrimming = (BiaTextTrimmingMode) e.NewValue;
+                    }));
+
+        #endregion
+
+        static BiaHyperlinkButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BiaHyperlinkButton),
+                new FrameworkPropertyMetadata(typeof(BiaHyperlinkButton)));
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            if (ActualWidth <= 1 ||
+                ActualHeight <= 1)
+                return;
+
+            var rounder = new LayoutRounder(this);
+            dc.DrawRectangle(Background ?? Brushes.Transparent, null, rounder.RoundRenderRectangle(false));
+
+            if (Content != null &&
+                Foreground != null)
+            {
+                var width = DefaultTextRenderer.Instance.Draw(
+                    this,
+                    Content,
+                    0, 0,
+                    Foreground,
+                    dc,
+                    ActualWidth,
+                    TextAlignment.Left,
+                    TextTrimming,
+                    true);
+
+                if (Foreground is SolidColorBrush brush)
+                {
+                    var y = rounder.RoundLayoutValue(ActualHeight - 1d);
+                    var pen = Caches.GetPen(brush.Color.ToByteColor(), 1d);
+                    
+                    dc.DrawLine(
+                        pen,
+                        new Point(0d, y), 
+                        new Point(width, y));
+                }
+            }
+        }
+
+        private double _textWidth;
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            return new Size((constraint.Width, _textWidth).Min(), DefaultTextRenderer.Instance.FontHeight);
+        }
+
+        private void UpdateSize()
+        {
+            var rounder = new LayoutRounder(this);
+            
+            _textWidth = string.IsNullOrEmpty(Content) ? 0 : rounder.RoundLayoutValue(Math.Ceiling(DefaultTextRenderer.Instance.CalcWidth(Content)));
+        }
+    }
+}
