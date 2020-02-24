9c7508d64dadaf980f5bf4e49df9d7a901dc559d
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-02-24 17:43:01 +0900

    Add CheckBoxPage

---
+++ Biaui/source/Biaui.ControlCatalog/Pages/CheckBoxPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Biaui.ControlCatalog.Pages
+{
+    public partial class CheckBoxPage
+    {
+        public CheckBoxPage()
+        {
+            Name = "CheckBox";
+            InitializeComponent();
+        }
+    }
+}
