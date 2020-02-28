65864f09850500392410484ed55e3618b780ff08
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-02-28 10:53:16 +0900

    Add ComboBoxPage

--- Biaui/source/Biaui.ControlCatalog/MainWindow.xaml.cs
+++ Biaui/source/Biaui.ControlCatalog/MainWindow.xaml.cs
@@ -52,7 +52,8 @@ namespace Biaui.ControlCatalog
                 new ToggleButtonPage(),
                 new CheckBoxPage(),
                 new RadioButtonPage(),
-                new TextBoxPage()
+                new TextBoxPage(),
+                new ComboBoxPage()
             };
 
             SelectedPage = Pages[0];
---
+++ Biaui/source/Biaui.ControlCatalog/Pages/ComboBoxPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Biaui.ControlCatalog.Pages
+{
+    public partial class ComboBoxPage
+    {
+        public string[] Items => new[]
+        {
+            "Africa",
+            "Asia",
+            "Europe",
+            "North America",
+            "South America",
+            "Antarctica",
+            "Australia"
+        };
+
+        #region SelectedItem
+
+        public string SelectedItem
+        {
+            get => (string) GetValue(SelectedItemsProperty);
+            set => SetValue(SelectedItemsProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedItemsProperty =
+            DependencyProperty.Register(
+                nameof(SelectedItem),
+                typeof(string),
+                typeof(ComboBoxPage));
+
+        #endregion
+
+        public Fruits[] EnumItems => Enum.GetValues(typeof(Fruits)).Cast<Fruits>().ToArray();
+        
+        #region SelectedEnumItem
+
+        public Fruits SelectedEnumItem
+        {
+            get => (Fruits) GetValue(SelectedEnumItemsProperty);
+            set => SetValue(SelectedEnumItemsProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedEnumItemsProperty =
+            DependencyProperty.Register(
+                nameof(SelectedEnumItem),
+                typeof(Fruits),
+                typeof(ComboBoxPage));
+
+        #endregion
+
+        public ComboBoxPage()
+        {
+            Name = "ComboBox";
+            InitializeComponent();
+
+            SelectedItem = Items[0];
+            SelectedEnumItem = EnumItems[0];
+        }
+    }
+
+    public enum Fruits
+    {
+        Apple,
+        Banana,
+        Orange,
+        Lemon
+    };
+
+    public class FruitsToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Fruits fruits))
+                return null;
+
+            // ReSharper disable StringLiteralTypo
+            return fruits switch
+            {
+                Fruits.Apple => "Pomme",
+                Fruits.Banana => "Banane",
+                Fruits.Orange => "Orange",
+                Fruits.Lemon => "Citron",
+                _ => throw new ArgumentOutOfRangeException()
+            };
+            // ReSharper restore StringLiteralTypo
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
