50c7f3015d84432e4af97f302784ccaa7201211d
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2023-07-02 10:05:01 +0900

    Improved readability

--- Biaui/source/Biaui/Controls/Converters/BoolInverseConverter.cs
+++ Biaui/source/Biaui/Controls/Converters/BoolInverseConverter.cs
@@ -8,23 +8,21 @@ public class BoolInverseConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        if (value is null)
-            return Binding.DoNothing;
-
-        if (value is bool b)
-            return Boxes.Bool(b == false);
-
-        return Binding.DoNothing;
+        return value switch
+        {
+            null => Binding.DoNothing,
+            bool b => Boxes.Bool(b == false),
+            _ => Binding.DoNothing
+        };
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        if (value is null)
-            return Binding.DoNothing;
-
-        if (value is bool b)
-            return Boxes.Bool(b == false);
-
-        return Binding.DoNothing;
+        return value switch
+        {
+            null => Binding.DoNothing,
+            bool b => Boxes.Bool(b == false),
+            _ => Binding.DoNothing
+        };
     }
 }
